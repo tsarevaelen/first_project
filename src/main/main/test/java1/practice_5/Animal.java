08b7e83ff2f1d18dcc3c68ff13beb418330d1844
tsarevaelen <olegovnae98@gmail.com>
2026-06-26 18:26:07 +0400

    Скорректировала структуру проекта, избавилась от ошибок

---
+++ src/main/main/test/java1/practice_5/Animal.java
@@ -0,0 +1,14 @@
+package test.java1.practice_5;
+
+public abstract class Animal {
+    private String species;
+    private int age;
+
+    public String getSpecies() {
+        return this.species;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
