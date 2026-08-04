3c2162e9dd97c4c4a0c3f8955b27b4f28751c435
tsarevaelen <olegovnae98@gmail.com>
2026-08-04 20:43:54 +0400

    Вернулась к изменениям в zoo

---
+++ src/main/main/practice_5/aquarium/Creature.java
@@ -0,0 +1,22 @@
+package practice_5.aquarium;
+
+public class Aquarium {
+    private String species;
+    private String move;
+
+    public Aquarium(String species, String move){
+        this.species = species;
+        this.move = move;
+    };
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getMove() {
+        return move;
+    }
+
+    @Override
+
+}
