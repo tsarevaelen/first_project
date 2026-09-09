4d1b5c30650767bd42a44098c8fdf161ab52ad84
tsarevaelen <olegovnae98@gmail.com>
2026-09-09 18:44:20 +0400

    внесены исправления в практике

--- src/main/main/practice_5/aquarium/Shark.java
+++ src/main/main/practice_5/aquarium/Shark.java
@@ -3,6 +3,6 @@ package practice_5.aquarium;
 public class Shark extends Creature implements Floating {
     @Override
     public void swim() {
-        System.out.println("Акула плавает быстро и агрессивно");
+        System.out.println("Акула плавает очень быстро и агрессивно");
     }
 }
